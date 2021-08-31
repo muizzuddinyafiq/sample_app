1893824eb2e06d1b1e4befed6e3a6d0635047439
muizzuddinyafiq <muizswift1234@gmail.com>
2021-08-31 17:38:48 +0800

    chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = "welcome_to_the_sample_app!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resourcers :users
   root 'static_pages#home'
   get 'static_pages/help'
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
 end
