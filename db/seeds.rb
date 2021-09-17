b71ebf3c1568f1cac89c4dde0433822a59a39a69
muizzuddinyafiq <muizswift1234@gmail.com>
2021-09-17 17:15:45 +0800

    chapter_11

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true, activated_at: Time.zone.now)
 
 User.create!(name: "Example User",
              email:"mailtommrr@gmail.com",
@@ -27,5 +27,6 @@ User.create!(name: "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated_at: Time.zone.now)
 end
